6414de68c5ad485cfe5bed7546de6153a937c04e
penguinlion <qetup1988@gmail.com>
2021-06-01 04:22:34 +0800

    add CSVWriter to csv.h and csv.cpp

--- csv.cpp
+++ csv.cpp
@@ -20,7 +20,7 @@ std::vector< std::string > CSVReader::getParsedLine() {
 		else {
 			getline( lineStream, cell, ',' );
 		}
-		std::cout << cell << "- -";
+		// std::cout << cell << "- -";	//just for dubugging
 		ret.push_back( cell );
 	}
 	return ret;
@@ -39,4 +39,45 @@ std::vector< std::string > CSVReader::getCol( int colNum ) {
 
 std::string CSVReader::getCell( int rowNum, int colNum ) {
 	return table.at( rowNum ).at( colNum );
+}
+
+CSVWriter::CSVWriter( const std::string& filename ) : fout( filename ) {}
+
+void CSVWriter::setRow( const int& rowNum, const std::vector< std::string >& row ) {
+	table.at( rowNum ).clear();
+	for ( auto& it : row )
+		table.at( rowNum ).push_back( it );
+}
+
+void CSVWriter::setCol( const int& colNum, const std::vector< std::string >& col ) {
+	if ( col.size() != table.size() )
+		throw std::invalid_argument( "CSVWriter: argument size is not equal to CSVWriter::table." );
+	for ( int i = 0; i < table.size(); i++ ) {
+		table.at( i ).at( colNum ) = col.at( colNum );
+	}
+}
+
+void CSVWriter::setCell( const int& rowNum, const int& colNum, const std::string& content ) {
+	table.at( rowNum ).at( colNum ) = content;
+}
+
+void CSVWriter::writeFile() {
+	std::string line;
+	for ( auto& row : table ) {
+		line = produceLine( row );
+		fout << line << std::endl;
+	}
+}
+
+std::string CSVWriter::produceLine( const std::vector< std::string >& row ) {
+	std::string rawLine = "";
+	for ( auto& it : row ) {
+		if ( !rawLine.empty() )
+			rawLine += ",";
+		if ( it.find( ',' ) != std::string::npos )
+			rawLine = "\"" + it + "\"";
+		else
+			rawLine += it;
+	}
+	return rawLine;
 }
--- csv.h
+++ csv.h
@@ -22,12 +22,17 @@ public:
 };
 
 class CSVWriter {
-	std::vector<std::vector<std::string> > table;
+	std::vector< std::vector< std::string > > table;
 	std::ofstream fout;
+	std::string produceLine( const std::vector< std::string >& row );
 
 public:
 	CSVWriter() = default;
-	CSVWriter( std::string_view filename );
+	CSVWriter( const std::string& filename );
+	void setRow( const int& rowNum, const std::vector< std::string >& row );  // setting functions only modify table, not write files.
+	void setCol( const int& colNum, const std::vector< std::string >& col );
+	void setCell( const int& rowNum, const int& colNum, const std::string& content );
+	void writeFile();
 };
 
 #endif
