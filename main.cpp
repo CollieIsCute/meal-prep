ba35db939c7bb1fb260f40df79684b078cf24827
penguinlion <qetup1988@gmail.com>
2021-06-01 04:38:42 +0800

    rm useless constructor in main.cpp

--- main.cpp
+++ main.cpp
@@ -14,66 +14,29 @@ date:2020/7/9
 #include <string_view>
 #include <vector>
 
-using worker = std::string;
-using workers = std::vector< std::string >;
+using person = std::string;
+using people = std::vector< person >;
 using date = std::string;
-using dates = std::vector< std::string >;
+using dates = std::vector< date >;
 const int DAILY_WORKING_PEOPLE = 3;
-
-class Group {
-private:
-	std::list< worker > people;
-
-public:
-	Group() = default;
-	void add( worker name );
-	void del( worker name );
-};
 
 class Calendar {
 private:
-	std::map< date, class Group > datesPeople;
-
+	std::map< date, people > datesPeople;
+	std::map< person, dates> peopleDates;
 public:
 	Calendar() = default;
 	Calendar( Calendar&& _cal );
-	Calendar( std::string_view filename );
 	void erase( const date& d );
-	Group& operator[]( const date& d );
-	const Group& operator[]( const date& d ) const;
 };
 
 int main() {
-	CSVReader c( "input.csv" );
-}
-
-Calendar::Calendar( std::string_view filename ) {
-	CSVReader r;
-}
-
-Calendar::Calendar( Calendar&& _cal ) {
-	datesPeople.swap( _cal.datesPeople );
+	CSVReader reader( "input.csv" );
+	Calendar workCal, attendCal;
+	const int NAME = 1, DATE = 7;
+	
 }
 
 void Calendar::erase( const date& d ) {
 	datesPeople.erase( d );
-}
-
-Group& Calendar::operator[]( const date& d ) {
-	return datesPeople.at( d );
-}
-
-const Group& Calendar::operator[]( const date& d ) const {
-	return datesPeople.at( d );
-}
-
-void Group::add( worker name ) {
-	if ( std::find( people.begin(), people.end(), name ) == people.end() )
-		people.push_back( name );
-}
-
-void Group::del( worker name ) {
-	auto index = std::find( people.begin(), people.end(), name );
-	if ( index != people.end() )
-		people.erase( index );
 }
